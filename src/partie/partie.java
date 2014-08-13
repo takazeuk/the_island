2e9b112048c17a57fd7639f54d28c5d70c259d80
TRABELSI <jonathan.trabelsi@hotmail.fr>
2014-08-13 23:53:16 +0200

    changement de l'interface graphique avec des espacements corrects. Changement des coordonnées corrects avec de simples tests if qui prend en paramètre le switch

--- src/partie/partie.java
+++ src/partie/partie.java
@@ -32,68 +32,110 @@ public class partie {
         
         //construction de la carte
         int compt=0;
-        int j;             
+        int j;       
+        int cordy;
         //transformer en boucle for
         while (compt!=13) {            
             switch(compt)
             {
                 case 0: case 12:
-                    for(j=2; j<9; j++)
+                    for(j=0; j<7; j++)
                     {
-                        tuiles terrain= new tuiles(compt, j, 0, 0);
+                        cordy=j+2;
+                        if (compt == 12)
+                        {
+                            cordy = j+8;
+                        }
+                        tuiles terrain= new tuiles(cordy, compt, 0, 0);
                         carte.add(terrain);
                         ImPan lenouv = new ImPan(0, 0, terrain);
                         lenouv.setSize(60, 60);
                         lenouv.setVisible(true);
                         mine.getContentPane().add(lenouv);
-                        lenouv.setLocation(150+j*60,compt*45);
+                        lenouv.setLocation(250+j*60,100+compt*45);
                         System.out.println("x="+ lenouv.terrain.x+ "y= "+ lenouv.terrain.y);
                         
                     }
                 break;
                 case 1: case 3: case 9: case 11:
-                    for(j=1; j<11; j++)
+                    for(j=0; j<10; j++)
                     {
-                        tuiles terrain= new tuiles(compt, j, 0, 0);
+                        cordy=j+1;
+                        if (compt == 3)
+                        {
+                            cordy = j+2;
+                        }
+                        if (compt == 9)
+                        {
+                            cordy = j+5;
+                        } 
+                        if (compt == 11)
+                        {
+                            cordy = j+6;
+                        }
+                        tuiles terrain= new tuiles(cordy, compt, 0, 0);
                         carte.add(terrain);
                         ImPan lenouv = new ImPan(0, 0, terrain);
                         lenouv.setSize(60, 60);
                         lenouv.setVisible(true);
                         mine.getContentPane().add(lenouv);
-                        lenouv.setLocation(120+j*60,compt*45);
+                        lenouv.setLocation(160+j*60,100+compt*45);
                         System.out.println("x="+ lenouv.terrain.x+ "y= "+ lenouv.terrain.y);
                     }
                 break;
                 case 2: case 4: case 6: case 8: case 10:
                     for(j=0; j<11; j++)
                     {
-                        tuiles terrain= new tuiles(compt, j, 0, 0);
+                        cordy=j+1;
+                        if (compt == 4)
+                        {
+                            cordy = j+2;
+                        }
+                        if (compt == 6)
+                        {
+                            cordy = j+3;
+                        } 
+                        if (compt == 8)
+                        {
+                            cordy = j+4;
+                        }
+                        if (compt == 10)
+                        {
+                            cordy = j+5;
+                        }
+                        tuiles terrain= new tuiles(cordy, compt, 0, 0);
                         carte.add(terrain);
                         ImPan lenouv = new ImPan(0, 0, terrain);
                         lenouv.setSize(60, 60);
                         lenouv.setVisible(true);
                         mine.getContentPane().add(lenouv);
-                        lenouv.setLocation(30+j*60,compt*45);
+                        lenouv.setLocation(130+j*60,100+compt*45);
                         System.out.println("x="+ lenouv.terrain.x+ "y= "+ lenouv.terrain.y);
                     }
                 break;
                 case 5: case 7:
                     for(j=0; j<12; j++)
                     {
-                        tuiles terrain= new tuiles(compt, j, 0, 0);
+                        cordy=j+2;
+                        if (compt == 7)
+                        {
+                            cordy = j+3;
+                        }
+                        
+                        tuiles terrain= new tuiles(cordy, compt, 0, 0);
                         carte.add(terrain);
                         ImPan lenouv = new ImPan(0, 0, terrain);
                         lenouv.setSize(60, 60);
                         lenouv.setVisible(true);
                         mine.getContentPane().add(lenouv);
-                        lenouv.setLocation(j*60,compt*45);
+                        lenouv.setLocation(100+j*60,100+compt*45);
                         System.out.println("x="+ lenouv.terrain.x+ "y= "+ lenouv.terrain.y);
                     }
             }
             compt++;
         }
         
-        mine.setSize(800, 800);
+        mine.setSize(950, 850);
         mine.setVisible(true);
         
         //on prépare le nombre de joueur
