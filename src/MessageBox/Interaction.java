5ab0328c7a53f37681d9bbdf6e41e4f947d46c1f
TRABELSI <jonathan.trabelsi@hotmail.fr>
2014-08-17 18:46:07 +0200

    Mise en place d'une classe qui intégre plusieurs type et renvoie de messages Box

---
+++ src/MessageBox/Interaction.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package MessageBox;
+import java.awt.Component;
+import java.util.Vector;
+import javax.swing.Icon;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Jonathan
+ */
+public class Interaction {
+    public static void affiche(String s) {
+        JOptionPane.showMessageDialog(null, s);
+    }
+
+    public static int demandeChoix(String s, Object[] tab) {
+        return JOptionPane.showOptionDialog(
+                null,
+                s,
+                "",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                tab,
+                tab[tab.length-1]
+        );
+    }
+
+    public static int demandeChoix(String s, Vector v) {
+        return demandeChoix(s, v.toArray());
+    }
+
+    public static int demandeChoix(String s) {
+        return Integer.parseInt(JOptionPane.showInputDialog(s));
+   }
+    public static String demandeChaine(String s) {
+        return JOptionPane.showInputDialog(s);
+    }
+    
+}
